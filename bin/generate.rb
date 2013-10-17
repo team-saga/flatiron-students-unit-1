9d64d44d4d20be1f99a632afba5f03977751f094
samueldowens <samueldowens@gmail.com>
2013-10-17 10:34:06 -0400

    working on ERB

--- bin/generate.rb
+++ bin/generate.rb
@@ -1,2 +1,21 @@
+require_relative '../config/environment'
+
 class SiteGenerator
+
+  index = ERB.new(File.open('lib/views/index.erb').read)
+  students = Student.all
+
+  File.open('_site/index.html', 'w+') do |f|
+    f << index.result(binding)
+  end
+
+  show = ERB.new(File.open('lib/views/show.erb').read)
+
+  student.each do |student|
+    File.open("_site/students/#{student.url}.html", 'w+') do |f|
+      f << show.result(binding)
+    end
+  end
+
+
 end
