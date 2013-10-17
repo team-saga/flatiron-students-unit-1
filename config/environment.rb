4bac6e66ec34c877e28acb567776592819bdc0d0
samueldowens <samueldowens@gmail.com>
2013-10-17 11:17:28 -0400

    Student Page Layout, almost done

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,6 @@ require 'open-uri'
 require 'pry'
 require 'sqlite3'
 require 'faker'
-
-Dir.foreach('lib') do |file|
-  next if file.start_with?('.')
-  require_relative '../lib/""' if file.end_with?('.rb')
-end
+require 'erb'
 require_relative '../lib/models/scraper'
 require_relative '../lib/models/student'
